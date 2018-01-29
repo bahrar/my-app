ba31a12953b9e5ffc5e0f91baba0bc9deaa0e106
Youyou <byouyou@gmail.com>
2018-01-29 23:53:07 +0100

    test echo file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #/bin/sh
 VERSION=$1
+if [ "$VERSION" == "deploy_user" ] ;then
+echo "oui deploy_user"
+else
+echo "non deploy_user"
+fi
 echo "deploying Version $VERSION..."
+echo "deploying Version $VERSION..." > toto.txt 
